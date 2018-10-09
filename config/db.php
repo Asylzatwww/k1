8bcd9385537586a0b94778e592ebfc09f19f28e8
Asylzatwww <Asylzatwww@yandex.ru>
2018-10-09 08:51:31 +0300

    db Connection

--- config/db.php
+++ config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=localhost;dbname=asylzatwww_k1',
+    'username' => 'asylzatwww_k1',
+    'password' => 'OEoHy4%*',
     'charset' => 'utf8',
 ];
