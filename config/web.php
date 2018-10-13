58f5a7f475fd23eb610432e582580d81388a5c04
Notebook <asylzatwww@yandex.ru>
2018-10-13 07:47:37 +0300

    rest

--- config/web.php
+++ config/web.php
@@ -38,14 +38,15 @@ $config = [
             ],
         ],
         'db' => require(__DIR__ . '/db.php'),
-        /*
+
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
+                ['class'=>'yii\rest\UrlRule', 'controller'=>'Poput']
             ],
         ],
-        */
+
     ],
     'params' => $params,
 ];
---
+++ controllers/PoputController.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: User
+ * Date: 13.10.2018
+ * Time: 7:41
+ */
+
+namespace app\controllers;
+
+use yii\rest\ActiveController;
+
+
+class PoputController extends ActiveController
+{
+    public $modelClass ='app\models\Poputka';
+}
