8209141a5d4dece1c57c8d8ff0cec4f2b3f7125e
Notebook <asylzatwww@yandex.ru>
2018-10-16 09:35:03 +0300

    rest

--- models/User.php
+++ models/User.php
@@ -116,7 +116,7 @@ class User extends \yii\db\ActiveRecord  implements IdentityInterface
      */
     public function getAuthKey()
     {
-        return $this->auth_key;
+        //return $this->auth_key;
     }
 
     /**
