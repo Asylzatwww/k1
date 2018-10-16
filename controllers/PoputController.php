760402d0130cb2e846e628d67f7a41ef1ed1dbe3
Notebook <asylzatwww@yandex.ru>
2018-10-16 09:38:13 +0300

    rest

--- controllers/PoputController.php
+++ controllers/PoputController.php
@@ -14,4 +14,15 @@ use yii\rest\ActiveController;
 class PoputController extends ActiveController
 {
     public $modelClass ='app\models\Poputka';
+
+    protected function verbs()
+    {
+        return [
+            'index' => ['GET', 'HEAD'],
+            'view' => ['GET', 'HEAD'],
+            'create' => ['POST'],
+            'update' => ['PUT', 'PATCH'],
+            'delete' => ['DELETE'],
+        ];
+    }
 }
