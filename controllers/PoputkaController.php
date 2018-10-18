51e70531e20a1ce19a5653827f8fbaf5a50bf360
User <asylzatwww@yandex.ru>
2018-10-18 07:56:58 +0300

    poputka

--- controllers/PoputkaController.php
+++ controllers/PoputkaController.php
@@ -55,8 +55,7 @@ class PoputkaController extends Controller
         return $dataProvider;
         */
 
-        return '
-[
+        return '[
         {
                 "id": "c200",
                 "name": "Ravi Tamada",
@@ -92,130 +91,8 @@ class PoputkaController extends Controller
                     "home": "00 000000",
                     "office": "00 000000"
                 }
-        },
-        {
-                "id": "c203",
-                "name": "John Wayne",
-                "email": "john_wayne@gmail.com",
-                "address": "xx-xx-xxxx,x - street, x - country",
-                "gender" : "male",
-                "phone": {
-                    "mobile": "+91 0000000000",
-                    "home": "00 000000",
-                    "office": "00 000000"
-                }
-        },
-        {
-                "id": "c204",
-                "name": "Angelina Jolie",
-                "email": "angelina_jolie@gmail.com",
-                "address": "xx-xx-xxxx,x - street, x - country",
-                "gender" : "female",
-                "phone": {
-                    "mobile": "+91 0000000000",
-                    "home": "00 000000",
-                    "office": "00 000000"
-                }
-        },
-        {
-                "id": "c205",
-                "name": "Dido",
-                "email": "dido@gmail.com",
-                "address": "xx-xx-xxxx,x - street, x - country",
-                "gender" : "female",
-                "phone": {
-                    "mobile": "+91 0000000000",
-                    "home": "00 000000",
-                    "office": "00 000000"
-                }
-        },
-        {
-                "id": "c206",
-                "name": "Adele",
-                "email": "adele@gmail.com",
-                "address": "xx-xx-xxxx,x - street, x - country",
-                "gender" : "female",
-                "phone": {
-                    "mobile": "+91 0000000000",
-                    "home": "00 000000",
-                    "office": "00 000000"
-                }
-        },
-        {
-                "id": "c207",
-                "name": "Hugh Jackman",
-                "email": "hugh_jackman@gmail.com",
-                "address": "xx-xx-xxxx,x - street, x - country",
-                "gender" : "male",
-                "phone": {
-                    "mobile": "+91 0000000000",
-                    "home": "00 000000",
-                    "office": "00 000000"
-                }
-        },
-        {
-                "id": "c208",
-                "name": "Will Smith",
-                "email": "will_smith@gmail.com",
-                "address": "xx-xx-xxxx,x - street, x - country",
-                "gender" : "male",
-                "phone": {
-                    "mobile": "+91 0000000000",
-                    "home": "00 000000",
-                    "office": "00 000000"
-                }
-        },
-        {
-                "id": "c209",
-                "name": "Clint Eastwood",
-                "email": "clint_eastwood@gmail.com",
-                "address": "xx-xx-xxxx,x - street, x - country",
-                "gender" : "male",
-                "phone": {
-                    "mobile": "+91 0000000000",
-                    "home": "00 000000",
-                    "office": "00 000000"
-                }
-        },
-        {
-                "id": "c2010",
-                "name": "Barack Obama",
-                "email": "barack_obama@gmail.com",
-                "address": "xx-xx-xxxx,x - street, x - country",
-                "gender" : "male",
-                "phone": {
-                    "mobile": "+91 0000000000",
-                    "home": "00 000000",
-                    "office": "00 000000"
-                }
-        },
-        {
-                "id": "c2011",
-                "name": "Kate Winslet",
-                "email": "kate_winslet@gmail.com",
-                "address": "xx-xx-xxxx,x - street, x - country",
-                "gender" : "female",
-                "phone": {
-                    "mobile": "+91 0000000000",
-                    "home": "00 000000",
-                    "office": "00 000000"
-                }
-        },
-        {
-                "id": "c2012",
-                "name": "Eminem",
-                "email": "eminem@gmail.com",
-                "address": "xx-xx-xxxx,x - street, x - country",
-                "gender" : "male",
-                "phone": {
-                    "mobile": "+91 0000000000",
-                    "home": "00 000000",
-                    "office": "00 000000"
-                }
         }
-    ]
-
-        ';
+    ]';
 
 
 
