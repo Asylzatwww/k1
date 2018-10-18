cd7e312fd9dc4971bb3b08ca5d396320e245029d
User <asylzatwww@yandex.ru>
2018-10-18 07:20:12 +0300

    poputka

--- controllers/PoputController.php
+++ controllers/PoputController.php
@@ -25,4 +25,41 @@ class PoputController extends ActiveController
             'delete' => ['DELETE'],
         ];
     }
+
+
+    public function actionSearch()
+    {
+        if (!empty($_GET)) {
+            $model = new $this->modelClass;
+            foreach ($_GET as $key => $value) {
+                if (!$model->hasAttribute($key)) {
+                    throw new \yii\web\HttpException(404, 'Invalid attribute:' . $key);
+                }
+            }
+            try {
+                $provider = new ActiveDataProvider([
+                    'query' => $model->find()->where($_GET),
+                    'pagination' => false
+                ]);
+            } catch (Exception $ex) {
+                throw new \yii\web\HttpException(500, 'Internal server error');
+            }
+
+            if ($provider->getCount() <= 0) {
+                throw new \yii\web\HttpException(404, 'No entries found with this query string');
+            } else {
+                return $provider;
+            }
+        } else {
+            throw new \yii\web\HttpException(400, 'There are no query string');
+        }
+
+        $model = new $this->modelClass;
+        $provider = new ActiveDataProvider([
+            'query' => $model->find()->where(["id"=>"1"]),
+            'pagination' => false
+        ]);
+
+        return $provider;
+    }
 }
