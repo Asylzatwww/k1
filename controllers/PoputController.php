4eca2fcb6ed43c4d741c7c123818be636a3092f9
User <asylzatwww@yandex.ru>
2018-10-18 07:31:53 +0300

    poputka

--- controllers/PoputController.php
+++ controllers/PoputController.php
@@ -10,6 +10,7 @@ namespace app\controllers;
 
 use yii\rest\ActiveController;
 use app\models\PoputkaSearch;
+use yii\data\ActiveDataProvider;
 
 
 
@@ -31,31 +32,6 @@ class PoputController extends ActiveController
 
     public function actionSearch()
     {
-        /*if (!empty($_GET)) {
-            $model = new $this->modelClass;
-            foreach ($_GET as $key => $value) {
-                if (!$model->hasAttribute($key)) {
-                    throw new \yii\web\HttpException(404, 'Invalid attribute:' . $key);
-                }
-            }
-            try {
-                $provider = new ActiveDataProvider([
-                    'query' => $model->find()->where($_GET),
-                    'pagination' => false
-                ]);
-            } catch (Exception $ex) {
-                throw new \yii\web\HttpException(500, 'Internal server error');
-            }
-
-            if ($provider->getCount() <= 0) {
-                throw new \yii\web\HttpException(404, 'No entries found with this query string');
-            } else {
-                return $provider;
-            }
-        } else {
-            throw new \yii\web\HttpException(400, 'There are no query string');
-        }*/
-
         $model = new $this->modelClass;
         $provider = new ActiveDataProvider([
             'query' => $model->find()->where(["id"=>1]),
