3d98d9d03bb0e4b86faa066a1bcb8c39ccb03ba4
User <asylzatwww@yandex.ru>
2018-10-29 09:49:19 +0300

    Touring Car

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -1,5 +1,4 @@
 <?php
-
 /* @var $this \yii\web\View */
 /* @var $content string */
 
@@ -7,7 +6,8 @@ use yii\helpers\Html;
 use yii\bootstrap\Nav;
 use yii\bootstrap\NavBar;
 use yii\widgets\Breadcrumbs;
-use app\assets\AppAsset;
+//use app\assets\AppAsset;
+use vendor\myVendorName\myPackageName\assets\AppAsset;
 
 AppAsset::register($this);
 ?>
