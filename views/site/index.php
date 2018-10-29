745a511bed741115255328bbc389f8af3cacafe8
User <asylzatwww@yandex.ru>
2018-10-29 11:12:00 +0300

    Touring Car

--- views/site/index.php
+++ views/site/index.php
@@ -3,6 +3,29 @@
 /* @var $this yii\web\View */
 
 $this->title = 'My Yii Application';
+
+/*
+ *
+ #page-header:before {
+    background-color: #FFFFFF !important;
+    background-image: url(http://www.touringcars.eu/sites/default/files/styles/1600x900/public/default_images/default_header_2.jpg?itok=YHp18irO) !important;
+    background-repeat: no-repeat !important;
+    background-attachment: scroll !important;
+    background-position: center top !important;
+}
+ * */
+
+$this->view->registerCss('
+ #page-header:before {
+    background-color: #FFFFFF !important;
+    background-image: url(http://www.touringcars.eu/sites/default/files/styles/1600x900/public/default_images/default_header_2.jpg?itok=YHp18irO) !important;
+    background-repeat: no-repeat !important;
+    background-attachment: scroll !important;
+    background-position: center top !important;
+}
+
+');
+
 ?>
 </div>
 
