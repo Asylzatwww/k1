48bf55fd7fe1529ba7bb94d9d9f7b6cf5571f37a
User <asylzatwww@yandex.ru>
2018-10-29 11:40:19 +0300

    Touring Car

--- views/site/about.php
+++ views/site/about.php
@@ -6,13 +6,201 @@ use yii\helpers\Html;
 
 $this->title = 'About';
 $this->params['breadcrumbs'][] = $this->title;
+
+$this->registerCss('
+ #page-header:before {
+    background-color: #FFFFFF !important;
+    background-image: url(../img/header-front-touringcars.jpg) !important;
+    background-repeat: no-repeat !important;
+    background-attachment: scroll !important;
+    background-position: center top !important;
+}
+
+');
+
+
 ?>
-<div class="site-about">
-    <h1><?= Html::encode($this->title) ?></h1>
 
-    <p>
-        This is the About page. You may modify the following file to customize its content:
-    </p>
 
-    <code><?= __FILE__ ?></code>
 </div>
+
+
+
+
+
+
+
+
+
+
+
+<div class="main-container container-fluid">
+
+    <header role="banner" id="page-header">
+
+        <div class="region region-header">
+            <section id="block-views-basic-header-block" class="block block-views container basic-header clearfix">
+
+
+                <div class="view view-basic-header view-id-basic_header view-display-id-block view-dom-id-dedf0e29294a1e187e0e13ef20b230f8">
+
+
+
+                    <div class="view-content">
+                        <div class="views-row views-row-1 views-row-odd views-row-first views-row-last">
+
+                            <div class="views-field views-field-title">        <span class="field-content col-xs-12 col-sm-8 col-md-7"><h1>ИДЕИ ДЛЯ ОТДЫXА</h1></span>  </div>  </div>
+                    </div>
+
+
+
+
+
+
+                </div>
+            </section>
+            <section id="block-block-13" class="block block-block clearfix fixed-top-nav">
+
+
+                <a href="http://booking.touringcars.eu" id="book-now-button"><img alt="" src="/sites/default/files/book-now-button.png" style="height:34px; width:148px"></a>
+
+            </section>
+        </div>
+    </header> <!-- /#page-header -->
+
+    <div class="row">
+
+
+        <section class="col-sm-12">
+
+            <a id="main-content"></a>
+            <div class="region region-content">
+                <section id="block-views-summary-block" class="block block-views container clearfix">
+
+
+                    <div class="view view-summary view-id-summary view-display-id-block view-dom-id-dbe2545a63fd4f6b2533bd1f973e3af3">
+
+
+
+                        <div class="view-content">
+                            <div class="views-row views-row-1 views-row-odd views-row-first views-row-last">
+
+                                <div class="views-field views-field-body col-xs-12 col-sm-9 col-md-7">        <div class="field-content"><p><strong>Путешествие по Северной Европе в арендованном кемпере - уникальный опыт. Почувствуйте &nbsp;на своей коже бриз чистого воздуха, полюбуйтесь удивительным Северным сиянием и в полной мере воспользуйтесь правом припарковать ваш кемпер в уголке нетронутой природы. Вот лишь некоторые идеи проведения неповторимого отдыха.</strong></p>
+                                    </div>  </div>
+                                <div class="views-field views-field-nothing col-xs-12 col-sm-3 col-md-5 pull-right" style="display: block;">        <span class="field-content">
+
+<a href="#routes" class="button bw">Best routes</a><a href="#offers" class="button bw">Best offers</a></span>  </div>  </div>
+                        </div>
+
+
+
+
+
+
+                    </div>
+                </section>
+                <section id="block-system-main" class="block block-system clearfix">
+
+
+                    <article id="node-70" class="node node-page clearfix" about="/ru/%D0%B8%D0%B4%D0%B5%D0%B8-%D0%B4%D0%BB%D1%8F-%D0%BE%D1%82%D0%B4%D1%8Bx%D0%B0" typeof="foaf:Document">
+                        <header>
+                            <span property="dc:title" content="ИДЕИ ДЛЯ ОТДЫXА" class="rdf-meta element-hidden"></span><span property="sioc:num_replies" content="0" datatype="xsd:integer" class="rdf-meta element-hidden"></span>      </header>
+                        <div class="field field-name-body field-type-text-with-summary field-label-hidden container"><div class="field-items"><div class="field-item even" property="content:encoded"></div></div></div>    </article>
+
+                </section>
+                <section id="block-block-9" class="block block-block views-list-title text-center clearfix">
+
+
+                    <h2><a class="anchor" href="#" id="routes" name="routes" title="Best routes">#</a>РЕКОМЕНДОВАННЫЕ МАРШРУТЫ</h2>
+                    <h3>ПРОСМОТРИТЕ ВЕЛИКОЛЕПНЫЕ МАРШРУТЫ</h3>
+
+                </section>
+                <section id="block-views-routes-block-2" class="block block-views container routes clearfix" style="visibility: visible; display: block;">
+
+
+                    <div class="view view-routes view-id-routes view-display-id-block_2 view-dom-id-a793ac3495c7de2df100bd30beeeb270">
+
+
+
+                        <div class="view-content" style="position: relative; width: 1140px; height: 1140px;">
+                            <div class="views-row views-row-1 views-row-odd views-row-first col-xs-12 col-sm-6 col-md-4" style="position: absolute; left: 0px; top: 0px;">
+
+                                <div class="views-field views-field-field-background-image">        <div class="field-content"><img class="adaptive-image img-responsive" typeof="foaf:Image" src="http://www.touringcars.eu/sites/default/files/styles/800x800/adaptive-image/public/background/scotland-1761292_1920_0.jpg?itok=gw2jAJqu" alt=""></div>  </div>
+                                <div class="views-field views-field-field-keywords">        <div class="field-content">latvia / Estonia</div>  </div>
+                                <div class="views-field views-field-field-category-title">        <div class="field-content">The Baltic route from Riga to Tallinn</div>  </div>
+                                <div class="views-field views-field-path">        <span class="field-content"><a href="/ru/riga-tallin-motorhome">&nbsp;</a></span>  </div>  </div>
+                            <div class="views-row views-row-2 views-row-even col-xs-12 col-sm-6 col-md-4" style="position: absolute; left: 380px; top: 0px;">
+
+                                <div class="views-field views-field-field-background-image">        <div class="field-content"><img class="adaptive-image img-responsive" typeof="foaf:Image" src="http://www.touringcars.eu/sites/default/files/styles/800x800/adaptive-image/public/background/krakow-1526667_1920_0.jpg?itok=S9hGH4e9" alt=""></div>  </div>
+                                <div class="views-field views-field-field-keywords">        <div class="field-content"></div>  </div>
+                                <div class="views-field views-field-field-category-title">        <div class="field-content">Camper holiday in Krakow</div>  </div>
+                                <div class="views-field views-field-path">        <span class="field-content"><a href="/ru/holiday-ideas/camper-holiday-krakow">&nbsp;</a></span>  </div>  </div>
+                            <div class="views-row views-row-3 views-row-odd col-xs-12 col-sm-6 col-md-4" style="position: absolute; left: 760px; top: 0px;">
+
+                                <div class="views-field views-field-field-background-image">        <div class="field-content"><img class="adaptive-image img-responsive" typeof="foaf:Image" src="http://www.touringcars.eu/sites/default/files/styles/800x800/adaptive-image/public/background/warsaw-996626_1920_0.jpg?itok=D3o_Za6v" alt=""></div>  </div>
+                                <div class="views-field views-field-field-keywords">        <div class="field-content"></div>  </div>
+                                <div class="views-field views-field-field-category-title">        <div class="field-content">Camper vacation in Warsaw</div>  </div>
+                                <div class="views-field views-field-path">        <span class="field-content"><a href="/ru/holiday-ideas/camper-vacation-warsaw">&nbsp;</a></span>  </div>  </div>
+                            <div class="views-row views-row-4 views-row-even col-xs-12 col-sm-6 col-md-4" style="position: absolute; left: 0px; top: 380px;">
+
+                                <div class="views-field views-field-field-background-image">        <div class="field-content"><img class="adaptive-image img-responsive" typeof="foaf:Image" src="http://www.touringcars.eu/sites/default/files/styles/800x800/adaptive-image/public/background/funfair-2369937_1920_0.jpg?itok=nAXE0JfI" alt=""></div>  </div>
+                                <div class="views-field views-field-field-keywords">        <div class="field-content"></div>  </div>
+                                <div class="views-field views-field-field-category-title">        <div class="field-content">Fun Family Camper Rental in Poland</div>  </div>
+                                <div class="views-field views-field-path">        <span class="field-content"><a href="/ru/holiday-ideas/family-route-poland">&nbsp;</a></span>  </div>  </div>
+                            <div class="views-row views-row-5 views-row-odd col-xs-12 col-sm-6 col-md-4" style="position: absolute; left: 380px; top: 380px;">
+
+                                <div class="views-field views-field-field-background-image">        <div class="field-content"><img class="adaptive-image img-responsive" typeof="foaf:Image" src="http://www.touringcars.eu/sites/default/files/styles/800x800/adaptive-image/public/background/castle-2535974_1920_0.jpg?itok=rUDxaMfv" alt=""></div>  </div>
+                                <div class="views-field views-field-field-keywords">        <div class="field-content"></div>  </div>
+                                <div class="views-field views-field-field-category-title">        <div class="field-content">Motorhome roadtrip in Poland </div>  </div>
+                                <div class="views-field views-field-path">        <span class="field-content"><a href="/ru/holiday-ideas/motorhome-roadtrip-poland">&nbsp;</a></span>  </div>  </div>
+                            <div class="views-row views-row-6 views-row-even col-xs-12 col-sm-6 col-md-4" style="position: absolute; left: 760px; top: 380px;">
+
+                                <div class="views-field views-field-field-background-image">        <div class="field-content"><img class="adaptive-image img-responsive" typeof="foaf:Image" src="http://www.touringcars.eu/sites/default/files/styles/800x800/adaptive-image/public/background/latvia-1944777_1920_0.jpg?itok=ZdHDc_l3" alt=""></div>  </div>
+                                <div class="views-field views-field-field-keywords">        <div class="field-content">latvia</div>  </div>
+                                <div class="views-field views-field-field-category-title">        <div class="field-content">Motorhome rental in versatile Latvia</div>  </div>
+                                <div class="views-field views-field-path">        <span class="field-content"><a href="/ru/holiday-ideas/motorhome-rental-latvia">&nbsp;</a></span>  </div>  </div>
+                            <div class="views-row views-row-7 views-row-odd col-xs-12 col-sm-6 col-md-4" style="position: absolute; left: 0px; top: 760px;">
+
+                                <div class="views-field views-field-field-background-image">        <div class="field-content"><img class="adaptive-image img-responsive" typeof="foaf:Image" src="http://www.touringcars.eu/sites/default/files/styles/800x800/adaptive-image/public/background/scotland-1761292_1920_0.jpg?itok=gw2jAJqu" alt=""></div>  </div>
+                                <div class="views-field views-field-field-keywords">        <div class="field-content">England</div>  </div>
+                                <div class="views-field views-field-field-category-title">        <div class="field-content">The Unique Scotland</div>  </div>
+                                <div class="views-field views-field-path">        <span class="field-content"><a href="/ru/holiday-ideas/camper-rental-scotland">&nbsp;</a></span>  </div>  </div>
+                            <div class="views-row views-row-8 views-row-even col-xs-12 col-sm-6 col-md-4" style="position: absolute; left: 380px; top: 760px;">
+
+                                <div class="views-field views-field-field-background-image">        <div class="field-content"><img class="adaptive-image img-responsive" typeof="foaf:Image" src="http://www.touringcars.eu/sites/default/files/styles/800x800/adaptive-image/public/background/port-106908_1280.jpg?itok=or13oebw" alt=""></div>  </div>
+                                <div class="views-field views-field-field-keywords">        <div class="field-content">Spain</div>  </div>
+                                <div class="views-field views-field-field-category-title">        <div class="field-content">The Charming Northern Spain</div>  </div>
+                                <div class="views-field views-field-path">        <span class="field-content"><a href="/ru/holiday-ideas/charming-northern-spain">&nbsp;</a></span>  </div>  </div>
+                            <div class="views-row views-row-9 views-row-odd views-row-last col-xs-12 col-sm-6 col-md-4" style="position: absolute; left: 760px; top: 760px;">
+
+                                <div class="views-field views-field-field-background-image">        <div class="field-content"><img class="adaptive-image img-responsive" typeof="foaf:Image" src="http://www.touringcars.eu/sites/default/files/styles/800x800/adaptive-image/public/background/skyscrapers-2935040_1920_0.jpg?itok=_vADYuwc" alt=""></div>  </div>
+                                <div class="views-field views-field-field-keywords">        <div class="field-content">latvia / riga</div>  </div>
+                                <div class="views-field views-field-field-category-title">        <div class="field-content">Latvia: The Cultural Riga</div>  </div>
+                                <div class="views-field views-field-path">        <span class="field-content"><a href="/ru/holiday-ideas/cultural-city-riga">&nbsp;</a></span>  </div>  </div>
+                        </div>
+
+
+
+
+
+
+                    </div>
+                </section>
+                <section id="block-block-8" class="block block-block views-list-title text-center clearfix">
+
+
+                    <h2><a class="anchor" href="#" id="offers" name="offers" title="Best offers">#</a>ЛУЧШИЕ ПРЕДЛОЖЕНИЯ</h2>
+                    <h3>ПРОСМОТРИТЕ СПЕЦИАЛЬНЫЕ ПРЕДЛОЖЕНИЯ</h3>
+
+                </section>
+            </div>
+        </section>
+
+
+    </div>
+</div>
+
+
+
+<div class="container">
+
