166c1a3f0730d47d846f13ba0ecb77d45e5f8e7f
User <asylzatwww@yandex.ru>
2018-10-29 09:20:58 +0300

    Touring Car

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -27,7 +27,7 @@ AppAsset::register($this);
 <div class="wrap">
     <?php
     NavBar::begin([
-        'brandLabel' => 'My Company',
+        'brandLabel' => '<img src="../img/touring-cars-logo.png" />My Company',
         'brandUrl' => Yii::$app->homeUrl,
         'options' => [
             'class' => 'navbar-inverse navbar-fixed-top',
