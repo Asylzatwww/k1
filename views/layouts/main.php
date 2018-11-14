2bdb3836b1a46f7b58db9e1de0f2fcd28988aba4
admin <asylzatwww@yandex.ru>
2018-11-14 10:56:14 +0000

    touringcars begins here

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -11,12 +11,6 @@ use app\assets\AppAsset;
 AppAsset::register($this);
 
 
-//$this->registerCssFile('../css/css_gypEgtlRvYu1WHmKes6mEXoj3Zf3sk2YcN4cIYKPUcI.css');
-//$this->registerCssFile('../css/css_lQaZfjVpwP_oGNqdtWCSpJT1EMqXdMiU84ekLLxQnc4.css');
-//$this->registerCssFile('../css/css_o5m5YfhNvCAmTYElqKpCQG6h1HjE4crK0Lriiytig8I.css');
-//$this->registerCssFile('../css/css_vHo3gQdCqF0aRTA3r6WfnlQfhq_O2_NZcDfs-xLFGPw.css');
-//$this->registerCssFile('../css/css_zy075VUTfcBGG737uKSc8dJGGp1HYV83QrGdFUMzk5Y.css');
-
 
 ?>
 <?php $this->beginPage() ?>
@@ -35,7 +29,7 @@ AppAsset::register($this);
 <div class="wrap">
     <?php
     NavBar::begin([
-        'brandLabel' => '<img src="../img/touring-cars-logo.png" style="margin-top: -25px" />',
+        'brandLabel' => 'VanTour',
         'brandUrl' => Yii::$app->homeUrl,
         'options' => [
             'class' => 'navbar-inverse navbar-fixed-top',
@@ -65,6 +59,45 @@ AppAsset::register($this);
     ]);
     NavBar::end();
     ?>
+
+
+
+    <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+        <!-- Indicators -->
+        <ol class="carousel-indicators">
+            <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+            <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+            <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+        </ol>
+
+        <!-- Wrapper for slides -->
+        <div class="carousel-inner" role="listbox">
+            <div class="item active">
+                <img src="http://asylzatwww.beget.tech/img/header-front-touringcars.jpg" alt="...">
+                <div class="carousel-caption">
+                    ...
+                </div>
+            </div>
+            <div class="item">
+                <img src="http://asylzatwww.beget.tech/img/header-front-touringcars.jpg" alt="...">
+                <div class="carousel-caption">
+                    ...
+                </div>
+            </div>
+            ...
+        </div>
+
+        <!-- Controls -->
+        <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+            <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+            <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+    </div>
+
 
     <div class="container">
         <?= Breadcrumbs::widget([
